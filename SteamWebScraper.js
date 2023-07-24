4de0ab9163ff9665e8f7e1977359d0c105b335b9
BioThrust <sciscor.x@gmail.com>
2023-07-24 15:51:48 -0500

    SteamWebScraper

--- SteamWebScraper.js
+++ SteamWebScraper.js
@@ -1,26 +1,38 @@
 const puppeteer = require('puppeteer');
 const express = require('express');
-
-async function login() {
+let page;
+async function login(steamID) {
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  page = await browser.newPage();
   await page.goto('https://steamcommunity.com/login/home/?goto=profiles%2F76561198368720362');
   await page.waitForTimeout(4000);
-  
+  console.log('reached page')
   await page.type('input[type="text"]', 'lsdjfklsdjflksdjfkl');
   await page.type('input[type="password"]', 'SsdfS8676j');
   await page.click('button[type="submit"]');
+  console.log('logged in')
   await page.waitForTimeout(4000);
   await page.screenshot({'path': 'oxylabs_js.png'})
-  await browser.close();
+
+}
+login()
+async function scrape(steamID) {
+  await page.goto(`https://steamcommunity.com/profiles/${steamID}/games/?tab=all`);
+  await page.waitForTimeout(1000);
+  await page.screenshot({'path': 'oxddylabs_js.png'});
+  let allGames = await page.$$eval('.gameslistitems_GamesListItemContainer_29H3o', games => games.map(game => game.textContent));
+  await page.screenshot({'path': 'oxddsdfylabs_js.png'});
+  console.log(allGames);
 }
 
 const app = express();
 
 // Define a route for triggering the login process
-app.get('/login', async (req, res) => {
+app.get('/:steamID', async (req, res) => {
   try {
-    await login();
+    const steamID = req.params.steamID;
+    console.log('request received')
+    await scrape(steamID)
     res.send('Login process completed.');
   } catch (err) {
     console.error('Error during login:', err);
