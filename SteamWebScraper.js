55a78171d0952b4357b8aa24796fe010d77c4970
BioThrust <sciscor.x@gmail.com>
2023-07-23 21:41:02 -0500

    ExpressJS addition

--- SteamWebScraper.js
+++ SteamWebScraper.js
@@ -1,5 +1,7 @@
 const puppeteer = require('puppeteer');
-async function Login() {
+const express = require('express');
+
+async function login() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://steamcommunity.com/login/home/?goto=profiles%2F76561198368720362');
@@ -12,4 +14,22 @@ async function Login() {
   await page.screenshot({'path': 'oxylabs_js.png'})
   await browser.close();
 }
-Login();
+
+const app = express();
+
+// Define a route for triggering the login process
+app.get('/login', async (req, res) => {
+  try {
+    await login();
+    res.send('Login process completed.');
+  } catch (err) {
+    console.error('Error during login:', err);
+    res.status(500).send('Error during login.');
+  }
+});
+
+// Start the server and listen on a specific port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running and listening on port ${port}.`);
+});
